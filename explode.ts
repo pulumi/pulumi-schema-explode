97eb6ab39430f38a1c3facbb7c02bb16c49a9434
Daniel Bradley <daniel@pulumi.com>
2022-09-26 11:56:56 +0100

    Add JSON output option
    Encapsulate writing into a context class.

--- explode.ts
+++ explode.ts
@@ -2,23 +2,23 @@ import {
   ComplexTypeSpecEnum,
   ComplexTypeSpecObject,
   FunctionSpec,
-  ObjectTypeSpec,
   ResourceSpec,
   Schema,
 } from "./schema";
-import { writeFileSync, existsSync, mkdirSync, open } from "fs";
+import { writeFileSync, existsSync, mkdirSync } from "fs";
 import yaml from "yaml";
 import path, { dirname } from "path";
-import {} from "@pulumi/pulumi";
+
+type FileWriter = (path: string, content: string) => void;
 
 export interface ExplodeOptions {
-  writeFile: (path: string, content: string) => void;
+  writeFile: FileWriter;
+  /** @default yaml */
+  format?: "yaml" | "json";
 }
 
-const yamlOptions: yaml.ToStringOptions = { lineWidth: 0, blockQuote: true };
-
 export function explode(schema: Schema, options: ExplodeOptions): void {
-  const { writeFile } = options;
+  const context = new Context(options);
   const {
     functions,
     types,
@@ -29,51 +29,73 @@ export function explode(schema: Schema, options: ExplodeOptions): void {
     ...remainingSchema
   } = schema;
 
-  writeFile("base.yaml", yaml.stringify(remainingSchema, yamlOptions));
+  context.writeData("base", remainingSchema);
   if (config !== undefined) {
-    writeFile("config.yaml", yaml.stringify(config, yamlOptions));
+    context.writeData("config", config);
   }
   if (provider !== undefined) {
-    writeFile("provider.yaml", yaml.stringify(provider, yamlOptions));
+    context.writeData("provider", provider);
   }
-  if (typeof language === "object" && language !== null) {
-    writeLanguages(language, options);
-  }
+  writeLanguages(language, context);
   for (const [token, fn] of Object.entries(functions ?? {})) {
-    writeTokenSpec("functions", token, fn, options);
+    writeTokenSpec("functions", token, fn, context);
   }
   for (const [token, resource] of Object.entries(resources ?? {})) {
-    writeTokenSpec("resources", token, resource, options);
+    writeTokenSpec("resources", token, resource, context);
   }
   for (const [token, type] of Object.entries(types ?? {})) {
-    writeTokenSpec("types", token, type, options);
+    writeTokenSpec("types", token, type, context);
   }
 }
 
-export function writeToFolder(dir: string): ExplodeOptions {
-  return {
-    writeFile: (filePath, content) => {
-      const combinedPath = path.join(dir, filePath);
-      const combinedDir = dirname(combinedPath);
-      if (!existsSync(combinedDir)) {
-        mkdirSync(combinedDir, { recursive: true });
-      }
-      return writeFileSync(combinedPath, content, {
-        encoding: "utf-8",
-      });
-    },
+export function fsFileWriter(dir: string): FileWriter {
+  return (filePath, content) => {
+    const combinedPath = path.join(dir, filePath);
+    const combinedDir = dirname(combinedPath);
+    if (!existsSync(combinedDir)) {
+      mkdirSync(combinedDir, { recursive: true });
+    }
+    return writeFileSync(combinedPath, content, {
+      encoding: "utf-8",
+    });
   };
 }
 
-function writeLanguages(language: unknown, options: ExplodeOptions) {
+type DataWriter = (pathWithoutExtension: string, data: any) => void;
+
+type Formatter = (data: any) => string;
+
+class Context {
+  constructor(options: ExplodeOptions) {
+    this.write = options.writeFile;
+    const format = options.format ?? "yaml";
+    this.ext = format;
+    this.format = format === "yaml" ? formatYaml : formatJson;
+    this.writeData = (pathWithoutExtension, data) => {
+      this.write(`${pathWithoutExtension}.${this.ext}`, this.format(data));
+    };
+  }
+
+  public write: FileWriter;
+  public format: Formatter;
+  public ext: string;
+  public writeData: DataWriter;
+}
+
+function formatJson(data: any) {
+  return JSON.stringify(data, null, 2);
+}
+
+function formatYaml(data: any) {
+  return yaml.stringify(data, { lineWidth: 0, blockQuote: true });
+}
+
+function writeLanguages(language: unknown, context: Context) {
   if (typeof language !== "object" || language == null) {
     return;
   }
   for (const [name, lang] of Object.entries(language)) {
-    options.writeFile(
-      `language.${name}.yaml`,
-      yaml.stringify(lang, yamlOptions)
-    );
+    context.writeData(`language.${name}`, lang);
   }
 }
 
@@ -81,18 +103,15 @@ function writeTokenSpec(
   type: "functions" | "types" | "resources",
   token: string,
   fn: FunctionSpec | ResourceSpec | ComplexTypeSpecObject | ComplexTypeSpecEnum,
-  options: ExplodeOptions
+  context: Context
 ): void {
   const [provider, modulePath, name] = token.split(":");
   const [module] = modulePath.split("/");
   const { description, spec } = formatForOutput(token, fn);
   const dir = path.join(module, type);
-  options.writeFile(
-    path.join(dir, `${name}.yaml`),
-    yaml.stringify(spec, yamlOptions)
-  );
+  context.writeData(path.join(dir, name), spec);
   if (description !== undefined) {
-    options.writeFile(path.join(dir, `${name}.md`), description);
+    context.write(path.join(dir, `${name}.md`), description);
   }
 }
 
--- scripts/test-examples.ts
+++ scripts/test-examples.ts
@@ -1,13 +1,13 @@
 import { readFileSync, writeFileSync } from "fs";
 import path from "path";
 import { Schema } from "../schema";
-import * as splitting from "../explode";
+import { explode, fsFileWriter } from "../explode";
 
 async function makeExample(example: string) {
   const dir = path.resolve("examples", example);
   const schemaPath = path.resolve(dir, "schema.json");
   const schema: Schema = JSON.parse(readFileSync(schemaPath, "utf-8"));
-  splitting.explode(schema, splitting.writeToFolder(dir));
+  explode(schema, { format: "yaml", writeFile: fsFileWriter(dir) });
 }
 
 makeExample("aws");
